4963eae3291f346438d44a16ce0f8166e87e0c38
RaditNaufalPradita <raditsmp77@gmail.com>
2021-04-07 21:39:59 -0700

    kode pertama

---
+++ xampp/htdocs/belajar_backend/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Kalkulator sederhana</title>
+	</head>
+	<body>
+		<form action="proses.php" method="post">
+			<input type="text" name="angka1">
+			<input type="text" name="angka2">
+			<input type="submit" name="submit" value="Tambahkan">
+		</form>
+
+	</body>
+</html>
